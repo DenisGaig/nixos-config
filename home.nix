f4c2acda6271447c7c42fc4da4fe96da8904dcbe
Denis <denis_gaignard@yahoo.fr>
2026-06-29 03:13:29 +0200

    feat: ajout import de modules

--- home.nix
+++ home.nix
@@ -3,6 +3,13 @@
   pkgs,
   ...
 }: {
+  imports = [
+    ./modules/fish.nix
+    ./modules/fzf.nix
+    ./modules/dunst.nix
+    ./modules/git.nix
+  ];
+
   home.username = "denis";
   home.homeDirectory = "/home/denis";
   # Ne JAMAIS changer cette valeur après le premier build
@@ -36,57 +43,6 @@
   ];
 
   # PROGRAMS
-  programs.fish = {
-    enable = true;
-
-    shellAliases = {
-      ll = "eza -lha --icons=auto --sort=name --group-directories-first";
-      ls = "eza --icons=auto";
-      ya = "yazi";
-      nv = "nvim";
-
-      g = "git";
-      gl = "git log";
-      gs = "git status";
-      ga = "git add";
-      gc = "git commit";
-      gpu = "git push";
-      gpl = "git pull";
-      gd = "git diff";
-      gco = "git checkout";
-      gb = "git branch";
-      gf = "git fetch";
-      gm = "git merge";
-    };
-
-    shellAbbrs = {
-      nrs = "sudo nixos-rebuild switch --flake /etc/nixos#denislab";
-      brain = "ssh toshiba -t 'cd ~/brain && nvim'";
-    };
-
-    interactiveShellInit = ''
-          set -U fish_greeting
-
-          fish_config theme choose "Dracula Official"
-
-          set -gx PNPM_HOME "$HOME/.local/share/pnpm"
-
-          if not string match -q -- $PNPM_HOME $PATH
-            set -gx PATH "$PNPM_HOME" $PATH
-          end
-
-          fish_add_path "$HOME/.npm-global/bin"
-
-          set show_file_or_dir_preview \
-        "if [ -d {} ]; then eza --tree --color=always {} | head -200; else bat -n --color=always --line-range :500 {}; fi"
-
-      set -gx FZF_CTRL_T_OPTS \
-        "--preview 'bash -c \"$show_file_or_dir_preview\"'"
-
-      set -gx FZF_ALT_C_OPTS \
-        "--preview 'eza --tree --color=always {} | head -200'"
-    '';
-  };
 
   programs.atuin = {
     enable = true;
@@ -94,22 +50,7 @@
   };
 
   programs.bat.enable = true;
-
   programs.btop.enable = true;
-
-  programs.fzf = {
-    enable = true;
-    enableFishIntegration = true;
-
-    defaultCommand = "fd --type f --hidden --exclude .git";
-
-    defaultOptions = [
-      "--preview-window=right:50%:wrap"
-    ];
-  };
-
-  programs.git.enable = true;
-
   programs.ripgrep.enable = true;
 
   programs.starship = {
@@ -122,52 +63,6 @@
   programs.zoxide = {
     enable = true;
     enableFishIntegration = true;
-  };
-
-  # SERVICES
-  services.dunst = {
-    enable = true;
-    settings = {
-      global = {
-        origin = "top-right";
-        offset = "12x12";
-        width = 320;
-        height = 120;
-        font = "HasklugNerdFont 11";
-        padding = 14;
-        horizontal_padding = 14;
-        corner_radius = 8;
-        frame_width = 2;
-        transparency = 10;
-        timeout = 5000;
-
-        # Dracula background + text
-        background = "#282a36";
-        foreground = "#f8f8f2";
-        frame_color = "#6272a4";
-      };
-
-      urgency_low = {
-        background = "#282a36";
-        foreground = "#6272a4";
-        frame_color = "#6272a4";
-        timeout = 3;
-      };
-
-      urgency_normal = {
-        background = "#282a36";
-        foreground = "#f8f8f2";
-        frame_color = "#bd93f9";
-        timeout = 5;
-      };
-
-      urgency_critical = {
-        background = "#ff5555";
-        foreground = "#f8f8f2";
-        frame_color = "#ff5555";
-        timeout = 0;
-      };
-    };
   };
 
   # MODULES

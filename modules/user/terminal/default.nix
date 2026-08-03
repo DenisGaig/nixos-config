2874f18b39890e55bb00a6f08f197436f7897dae
Denis <denis_gaignard@yahoo.fr>
2026-08-03 16:52:01 +0200

    feat: séparation terminal et shell

--- modules/user/terminal/default.nix
+++ modules/user/terminal/default.nix
@@ -1,32 +1,5 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
-    calc
-    curl
-    curl.dev
-    eza
-    fd
     kitty
-    wl-clipboard
   ];
-
-  programs.atuin = {
-    enable = true;
-    enableFishIntegration = true;
-  };
-
-  programs.bat.enable = true;
-  programs.btop.enable = true;
-  programs.ripgrep.enable = true;
-
-  programs.starship = {
-    enable = true;
-    enableFishIntegration = true;
-  };
-
-  programs.yazi.enable = true;
-
-  programs.zoxide = {
-    enable = true;
-    enableFishIntegration = true;
-  };
 }

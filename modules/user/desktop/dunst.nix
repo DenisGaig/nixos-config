08778632a332cbd61a72468b1888741d7687b58f
Denis <denis_gaignard@yahoo.fr>
2026-08-04 03:22:20 +0200

    refacto: ajout dunst comme module enable depuis desktop

--- modules/user/desktop/dunst.nix
+++ modules/user/desktop/dunst.nix
@@ -1,45 +1,58 @@
-{...}: {
-  services.dunst = {
-    enable = true;
-    settings = {
-      global = {
-        origin = "top-right";
-        offset = "12x12";
-        width = 320;
-        height = 120;
-        font = "HasklugNerdFont 11";
-        padding = 14;
-        horizontal_padding = 14;
-        corner_radius = 8;
-        frame_width = 2;
-        transparency = 10;
-        timeout = 5000;
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}: let
+  cfg = config.userSettings.desktop.dunst;
+in {
+  options.userSettings.desktop.dunst = {
+    enable = lib.mkEnableOption "Install dunst";
+  };
 
-        # Dracula background + text
-        background = "#282a36";
-        foreground = "#f8f8f2";
-        frame_color = "#6272a4";
-      };
+  config = lib.mkIf cfg.enable {
+    services.dunst = {
+      enable = true;
+      settings = {
+        global = {
+          origin = "top-right";
+          offset = "12x12";
+          width = 320;
+          height = 120;
+          font = "HasklugNerdFont 11";
+          padding = 14;
+          horizontal_padding = 14;
+          corner_radius = 8;
+          frame_width = 2;
+          transparency = 10;
+          timeout = 5000;
 
-      urgency_low = {
-        background = "#282a36";
-        foreground = "#6272a4";
-        frame_color = "#6272a4";
-        timeout = 3;
-      };
+          # Dracula background + text
+          background = "#282a36";
+          foreground = "#f8f8f2";
+          frame_color = "#6272a4";
+        };
 
-      urgency_normal = {
-        background = "#282a36";
-        foreground = "#f8f8f2";
-        frame_color = "#bd93f9";
-        timeout = 5;
-      };
+        urgency_low = {
+          background = "#282a36";
+          foreground = "#6272a4";
+          frame_color = "#6272a4";
+          timeout = 3;
+        };
 
-      urgency_critical = {
-        background = "#ff5555";
-        foreground = "#f8f8f2";
-        frame_color = "#ff5555";
-        timeout = 0;
+        urgency_normal = {
+          background = "#282a36";
+          foreground = "#f8f8f2";
+          frame_color = "#bd93f9";
+          timeout = 5;
+        };
+
+        urgency_critical = {
+          background = "#ff5555";
+          foreground = "#f8f8f2";
+          frame_color = "#ff5555";
+          timeout = 0;
+        };
       };
     };
   };

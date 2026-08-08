70e98cc1f14d5879497bde7c262e70ed1b3c54ae
Denis <denis_gaignard@yahoo.fr>
2026-08-08 02:04:16 +0200

    feat: ajout nix.optimise.aytomatic=true

--- modules/system/nix.nix
+++ modules/system/nix.nix
@@ -6,6 +6,12 @@
     trusted-users = ["root" "denis"];
   };
 
+  # Optimisation automatique du store Nix : Nix recherche les fichiers
+  # identiques présents plusieurs fois dans /nix/store et les remplace par
+  # des hard links. Cela peut réduire l'espace disque utilisé sans modifier
+  # le contenu de tes générations.
+  nix.optimise.automatic = true;
+
   # Permet l'exécution de binaires ELF génériques (non compilés pour NixOS)
   # ex: binaires téléchargés par neovim (LSP, windsurf/neocodeium...)
   # sans cela, ces binaires échouent car /lib/ld-linux-x86-64.so.2 est absent
